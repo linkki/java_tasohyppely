8ab14daac1edf4b88febdbf24642b84442745f2a
Cherubi <jenna.tuominen@cs.helsinki.fi>
2019-06-09 16:38:33 +0300

    Kartta_6

--- src/tasohyppelymalli/Pelialue.java
+++ src/tasohyppelymalli/Pelialue.java
@@ -8,6 +8,7 @@ package tasohyppelymalli;
 import java.awt.Color;
 import java.awt.Point;
 import java.util.HashMap;
+import java.util.Scanner;
 import javax.swing.JPanel;
 
 /**
@@ -31,5 +32,25 @@ public class Pelialue extends JPanel {
         kentta += "imipimii" + "\n";
         kentta += "iimmmiii" + "\n";
         kentta += "iiiiiiii" + "\n";
+        
+        Scanner lukija = new Scanner(kentta);
+        int x = 0, y = 0;
+        while (lukija.hasNextLine()) {
+            String rivi = lukija.nextLine();
+            for (char c : rivi.toCharArray()) {
+                if (c == 'c') {}
+                if (c == 'm') {
+                    hahmolista.put(new Maapala(), new Point(x, y));
+                }
+                if (c == 'p') {
+                    
+                }
+                
+                x += 50; //hahmojen leveys
+            }
+            
+            y += 50; //hahmojen korkeus
+            x = 0;
+        }
     }
 }
